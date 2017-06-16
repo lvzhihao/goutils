f3ece4da3ed280217b2cc4841a4b9c01e5ef6144
edwin <edwin.lzh@gmail.com>
2017-06-16 19:46:43 +0800

    fix

--- rand.go
+++ rand.go
@@ -1,7 +1,11 @@
 package goutils
 
 import (
+	"fmt"
 	"math/rand"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -15,4 +19,44 @@ func RandomString(strlen int) string {
 	}
 
 	return string(result)
+}
+
+/*
+ * runtime version >= version; return true
+ * runtime version <  version; return false
+ */
+func CompareRuntimeVersion(version string) bool {
+	switch CompareVersion(strings.Replace(runtime.Version(), "go", "", -1), strings.Replace(version, "go", "", -1), 2) {
+	case "<":
+		return false
+	default:
+		return true
+	}
+}
+
+func CompareVersion(v1, v2 string, width int) string {
+	v1s := parseVersion(v1, width)
+	v2s := parseVersion(v2, width)
+	if v1s > v2s {
+		return ">"
+	} else if v1s < v2s {
+		return "<"
+	} else {
+		return "="
+	}
+}
+
+func parseVersion(version string, width int) int64 {
+	vList := strings.Split(version, ".")
+	format := fmt.Sprintf("%%s%%0%ds", width)
+	v := ""
+	for _, value := range vList {
+		v = fmt.Sprintf(format, v, value)
+	}
+	rst, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		return 0
+	} else {
+		return rst
+	}
 }
